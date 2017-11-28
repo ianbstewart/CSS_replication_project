e24717b40b2b00bc40a7574799b1a7cb4a508205
Yuval Pinter <uvp@gatech.edu>
2017-11-28 11:00:23 -0500

    permutation tests for #7

--- scripts/experiment_1/exp11.py
+++ scripts/experiment_1/exp11.py
@@ -2,11 +2,14 @@ from __future__ import division
 import json
 import csv
 import pandas as pd
+import progressbar
 from collections import defaultdict, Counter
+from random import shuffle
 
 LANG_ID_FILE = '../../data/tweets/archive_Jan-01-17_Oct-31-17_ref_hashtags_filtered_langid.csv'
 TWEETS_FILE = '../../data/tweets/archive_Jan-01-17_Oct-31-17_ref_hashtags_filtered.json'
 USERS_FILE = '../../data/user_groups.csv'
+PERMUTATIONS = 100000
 
 # cache lang ids
 lang_ids = {}
@@ -19,9 +22,13 @@ with open(LANG_ID_FILE) as lang_id_file:
         if l == 'es' or l == 'ca':
             lang_ids[int(t)] = l
 
+print 'read {} language ids'.format(len(lang_ids))
+
 # get users
 users = pd.read_csv(USERS_FILE)
 user_ids = users['username'].tolist()
+
+print 'read {} user assignments'.format(len(users))
 
 # mine tweets
 user_langs = defaultdict(Counter)
@@ -35,12 +42,36 @@ with open(TWEETS_FILE) as tweets_file:
             if id in lang_ids:
                 user_langs[user][lang_ids[id]] += 1
 
+print 'read {} tweets in ca/es from users'.format(\
+            sum([sum(ls.values()) for u,ls in user_langs.items()]))
+
 def ca_rate(u):
     if u not in user_langs:
         return float('NaN')
     return user_langs[u]['ca'] / sum(user_langs[u].values())
 
 users['ca_rate'] = [ca_rate(u) for u in user_ids]
-print 'YES:', users[users['group'] == 'YES']['ca_rate'].mean()
-print 'NO:', users[users['group'] == 'NO']['ca_rate'].mean()
-print users[:10]
+
+def phats(df=users, gid='group'):
+    phat_yes = df[df[gid] == 'YES']['ca_rate'].mean()
+    phat_no = df[df[gid] == 'NO']['ca_rate'].mean()
+    yn_d = phat_yes - phat_no
+    return phat_yes, phat_no, yn_d
+    
+true_ph_yes, true_ph_no, true_yn_d = phats()
+print 'YES:', true_ph_yes
+print 'NO:', true_ph_no
+print 'd:', true_yn_d
+
+print 'Performing permutation test'
+bar = progressbar.ProgressBar()
+d_larger = 0
+group_assigns = users['group'].tolist()
+for i in bar(xrange(PERMUTATIONS)):
+    shuffle(group_assigns)
+    users['s_group'] = group_assigns
+    _, _, d_i = phats(users, 's_group')
+    if d_i > true_yn_d:
+        d_larger += 1
+
+print 'p:', d_larger / PERMUTATIONS
