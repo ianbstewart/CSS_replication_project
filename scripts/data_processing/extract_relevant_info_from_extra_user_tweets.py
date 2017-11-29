4bafe158e1f4d20005ca4ffb1c58716daa07f727
istewart6 <istewart6@gatech.edu>
2017-11-28 20:03:06 -0500

    starting experiment 2 and fixing user info extraction

--- scripts/data_processing/extract_relevant_info_from_extra_user_tweets.py
+++ scripts/data_processing/extract_relevant_info_from_extra_user_tweets.py
@@ -21,39 +21,36 @@ def main():
     
     ## load data
     # pro_anti_hashtags = pd.read_csv(pro_anti_file, sep=',', index_col=False)
-    expanded_hashtags = pd.read_csv(expanded_hashtag_file, sep=',', index_col=False)
+    expanded_hashtags = pd.read_csv(expanded_hashtag_file, sep=',', index_col=False).loc[:, 'expanded']
     expanded_hashtag_set = set(expanded_hashtags)
+    # print(expanded_hashtag_set)
     # remapping
-    #hashtags_joined = pd.merge(pro_anti_hashtags, expanded_hashtags, left_on='hashtag', right_on='original')
-    #hashtag_sentiment_lookup = dict(zip(hashtags_joined.loc[:,'expanded'].values.tolist(), hashtags_joined.loc[:, 'sentiment']))
-    #print(hashtag_sentiment_lookup.items()[:10])
     
     ## process data
     data = []
-    hashtag_matcher = re.compile('(?<=#)\w+')
+    hashtag_matcher = re.compile('(?<=#)[^# ]+')
     rt_matcher = re.compile('^rt[ :]')
     space_matcher = re.compile('(\s+|\n|\t)')
     # cutoff = 10000
     with codecs.open(user_tweet_file, 'r', encoding='utf-8') as user_tweet_input:
         for i, l in enumerate(user_tweet_input):
+            l = l.strip()
             try:
                 t = json.loads(l)
                 # remove spaces
                 t_text = space_matcher.sub(' ',t['text'])
                 t_hashtags = hashtag_matcher.findall(t_text)
                 t_contains_ref_hashtag = int(len(set(t_hashtags) & expanded_hashtag_set) > 0)
-                t_lang = classify(t_text)[0]
+                t_lang, t_lang_conf = classify(t_text)
                 t_is_rt = int(len(rt_matcher.findall(t_text.lower())) > 0)
-                t_data = [t['id'], t['user']['screen_name'], t_text, ','.join(t_hashtags), t_contains_ref_hashtag, t_lang, t_is_rt]
+                t_data = [t['id'], t['user']['screen_name'], t_text, ','.join(t_hashtags), t_contains_ref_hashtag, t_lang, t_lang_conf, t_is_rt]
                 data.append(t_data)
                 # extract hashtags
             except Exception, e:
-                print(e)
-                break
                 pass
             if(i % 100000 == 0):
                 print('processed %d tweets'%(i))
-    col_names = ['id', 'user', 'text', 'hashtags', 'contains_ref_hashtag', 'lang', 'retweeted']
+    col_names = ['id', 'user', 'text', 'hashtags', 'contains_ref_hashtag', 'lang', 'lang_conf', 'retweeted']
     data = pd.DataFrame(data, columns=col_names)
     
     ## write to file
