af420c7148a1984a0f938137b33e4cc03bb91863
istewart6 <istewart6@gatech.edu>
2018-04-02 18:12:44 -0400

    filtered control tweets and fixed experiment 2

--- scripts/data_processing/mine_extra_user_tweets.py
+++
@@ -1,5 +0,0 @@
-"""
-Mine extra tweets from archive for specified users.
-THIS WILL TAKE A WHILE.
-"""
-import argparse
--- scripts/experiment_2/experiment_2.py
+++ scripts/experiment_2/experiment_2.py
@@ -13,9 +13,12 @@ def run_compare_test(tweet_data_1, tweet_data_2):
     relevant_users = set(tweet_data_1.loc[:, 'user'].unique()) & set(tweet_data_2.loc[:, 'user'].unique())
     tweet_data_relevant_1 = tweet_data_1[tweet_data_1.loc[:, 'user'].isin(relevant_users)]
     tweet_data_relevant_2 = tweet_data_2[tweet_data_2.loc[:, 'user'].isin(relevant_users)]
-    logging.info('%d tweets in data 1'%(tweet_data_relevant_1.shape[0]))
-    logging.info('%d tweets in data 2'%(tweet_data_relevant_2.shape[0]))
-    logging.info('%d relevant users'%(len(relevant_users)))
+    tweet_count_1 = tweet_data_relevant_1.shape[0]
+    tweet_count_2 = tweet_data_relevant_2.shape[0]
+    user_count = len(relevant_users)
+    logging.info('%d tweets in data 1'%(tweet_count_1))
+    logging.info('%d tweets in data 2'%(tweet_count_2))
+    logging.info('%d relevant users'%(user_count))
     ca_1 = tweet_data_relevant_1.groupby('user').apply(lambda x: (x.loc[:, 'lang']=='ca').astype(int).sum() / x.shape[0])
     ca_2 = tweet_data_relevant_2.groupby('user').apply(lambda x: (x.loc[:, 'lang']=='ca').astype(int).sum() / x.shape[0])
     ca_use_diff = ca_1 - ca_2
@@ -25,16 +28,24 @@ def run_compare_test(tweet_data_1, tweet_data_2):
     h_0 = 0.
     t_stat, p_val = ttest_1samp(ca_use_diff, h_0)
     logging.info('d_u = %.5f, err = %.5f, t=%.3f (p=%.3E)'%(d_u, d_u_err, t_stat, p_val))
-    return d_u, d_u_err, t_stat, p_val
+    stats = pd.Series([tweet_count_1, tweet_count_2, user_count], index=['treatment_tweets', 'control_tweets', 'users'])
+    results = pd.Series([d_u, d_u_err, t_stat, p_val], index=['d_u', 'd_u_err', 't', 'p'])
+    return stats, results
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--tweet_file', default='../../data/tweets/extra_user_tweets/Jan-01-17_Oct-31-17_user_tweets.tsv')
+    parser.add_argument('--tweet_file', default='../../data/tweets/extra_user_tweets/Jan-01-17_Oct-31-17_user_tweets_final.tsv')
+    parser.add_argument('--out_dir', default='../../output')
     args = parser.parse_args()
     tweet_file = args.tweet_file
-    if(not os.path.exists('../../output/')):
-        os.mkdir('../../output/')
-    logging.basicConfig(level=logging.INFO, filemodel='w', format='%(message)s', filename='../../output/experiment_2.txt')
+    out_dir = args.out_dir
+    if(not os.path.exists(out_dir)):
+        os.mkdir(out_dir)
+    logging.basicConfig(level=logging.INFO, filemodel='w', format='%(message)s', filename=os.path.join(out_dir,'experiment_2.txt'))
+    stats_out_file = os.path.join(out_dir, 'experiment_2_tweet_user_counts.tsv')
+    results_out_file = os.path.join(out_dir, 'experiment_2_results.tsv')
+    all_stats = pd.DataFrame()
+    all_results = pd.DataFrame()
 
     ## load data
     tweet_data = pd.read_csv(tweet_file, sep='\t', index_col=False)
@@ -51,77 +62,51 @@ def main():
     allowed_langs = set(['es', 'ca'])
     tweet_data_original_high_conf = tweet_data_original[(tweet_data_original.loc[:, 'lang_conf'] >= lang_conf_cutoff) &
                                                         (tweet_data_original.loc[:, 'lang'].isin(allowed_langs))]
-    logging.info('%d relevant tweets'%(tweet_data_original_high_conf.shape[0]))
     # restrict to relevant users
     relevant_users = tweet_data_original_high_conf.groupby('user').apply(lambda x: (x.loc[:, 'contains_ref_hashtag'].max()==1 and 
                                                                                     x.loc[:, 'contains_ref_hashtag'].min()==0))
     relevant_users = relevant_users[relevant_users].index.tolist()
-    logging.info('%d relevant users'%(len(relevant_users)))
     tweet_data_relevant = tweet_data_original_high_conf[tweet_data_original_high_conf.loc[:, 'user'].isin(relevant_users)]
-    logging.info('%d relevant tweets'%(tweet_data_relevant.shape[0]))
 
     ## split into referendum and control data
     logging.info('starting referendum versus control test')
     tweet_data_ref = tweet_data_relevant[tweet_data_relevant.loc[:, 'contains_ref_hashtag'] == 1]
     tweet_data_control = tweet_data_relevant[tweet_data_relevant.loc[:, 'contains_ref_hashtag'] == 0]
-    logging.info('%d referendum tweets'%(tweet_data_ref.shape[0]))
-    logging.info('%d non-referendum tweets'%(tweet_data_control.shape[0]))
-    logging.info('%d users'%(tweet_data_relevant.loc[:, 'user'].nunique()))
-    logging.info('referendum tweet language distribution')
-    logging.info(tweet_data_ref.loc[:, 'lang'].value_counts())
-    logging.info('control tweet language distribution')
-    logging.info(tweet_data_control.loc[:, 'lang'].value_counts())
 
     ### first test: referendum versus non-referendum tweets
-
     ## compute probability of Catalan in referendum and control tweets
-    test_lang = 'ca'
-    compute_prob_lang = lambda x: x[x.loc[:, 'lang'] == test_lang].shape[0] / x.shape[0]
-    cat_prob_ref = tweet_data_ref.groupby('user').apply(compute_prob_lang)
-    cat_prob_control = tweet_data_control.groupby('user').apply(compute_prob_lang)
-    all_control_d_u = cat_prob_ref - cat_prob_control
-    all_control_d_u_mean = all_control_d_u.mean()
-    all_control_d_u_stderr = all_control_d_u.std() / len(all_control_d_u)**.5
-    logging.info('d_u for all control tweets is %.3f +/- %.3f'%(all_control_d_u_mean, all_control_d_u_stderr))
-    
-    ## test for significance
-    d_u_null = 0.
-    t_stat, p_val = ttest_1samp(all_control_d_u, d_u_null)
-    logging.info('significance: t=%.3f p=%.3E'%(t_stat, p_val))
+    stats, results = run_compare_test(tweet_data_ref, tweet_data_control)
+    all_stats = all_stats.append(pd.DataFrame(stats).transpose())
+    all_results = all_results.append(pd.DataFrame(results).transpose())
 
     ### second test: referendum versus non-referendum tweets with hashtags
     logging.info('starting referendum versus hashtag control test')
 
-    ## re-segment data
+    ## re-segment data, relevant users
     tweet_data_with_hashtags = tweet_data_original_high_conf[tweet_data_original_high_conf.loc[:, 'hashtag_count'] > 0]
-    # recompute relevant users
     relevant_users = tweet_data_with_hashtags.groupby('user').apply(lambda x: (x.loc[:, 'contains_ref_hashtag'].max()==1 and 
                                                                                x.loc[:, 'contains_ref_hashtag'].min()==0))
     relevant_users = relevant_users[relevant_users].index.tolist()
-    # recompute relevant data
     tweet_data_relevant_with_hashtags = tweet_data_with_hashtags[tweet_data_with_hashtags.loc[:, 'user'].isin(relevant_users)]
     tweet_data_ref = tweet_data_relevant_with_hashtags[tweet_data_relevant_with_hashtags.loc[:, 'contains_ref_hashtag'] == 1]
     tweet_data_control = tweet_data_relevant_with_hashtags[tweet_data_relevant_with_hashtags.loc[:, 'contains_ref_hashtag'] == 0]
-    logging.info('%d referendum tweets'%(tweet_data_ref.shape[0]))
-    logging.info('%d non-referendum tweets'%(tweet_data_control.shape[0]))
-    logging.info('%d users'%(tweet_data_relevant_with_hashtags.loc[:, 'user'].nunique()))
-    cat_prob_ref = tweet_data_ref.groupby('user').apply(compute_prob_lang)
-    cat_prob_control = tweet_data_control.groupby('user').apply(compute_prob_lang)
-    hashtag_control_d_u = cat_prob_ref - cat_prob_control
-    hashtag_control_d_u_mean = hashtag_control_d_u.mean()
-    hashtag_control_d_u_stderr = hashtag_control_d_u.std() / len(hashtag_control_d_u)**.5
-    logging.info('d_u for all control hashtag tweets is %.3f +/- %.3f'%(hashtag_control_d_u_mean, hashtag_control_d_u_stderr))
-    d_u_null = 0.
-    t_stat, p_val = ttest_1samp(hashtag_control_d_u, d_u_null)
-    logging.info('significance: t=%.3f p=%.3E'%(t_stat, p_val))
+    stats, results = run_compare_test(tweet_data_ref, tweet_data_control)
+    all_stats = all_stats.append(pd.DataFrame(stats).transpose())
+    all_results = all_results.append(pd.DataFrame(results).transpose())
 
     ## third test: @-replies versus non-replies with hashtag
-    #reply_data = tweet_data_original[(tweet_data_original.loc[:, 'reply']==1) & (tweet_data_original.loc[:, 'hashtag_count']==0)]
-    #hashtag_data = tweet_data_original[(tweet_data_original.loc[:, 'reply']==0) & (tweet_data_original.loc[:, 'hashtag_count']>0)]
     logging.info('starting reply versus not-reply test')
     reply_data = tweet_data_original_high_conf[(tweet_data_original_high_conf.loc[:, 'reply']==1) & (tweet_data_original_high_conf.loc[:, 'hashtag_count']==0)]
     hashtag_data = tweet_data_original_high_conf[(tweet_data_original_high_conf.loc[:, 'reply']==0) & (tweet_data_original_high_conf.loc[:, 'hashtag_count']>0)]
-    test_results = run_compare_test(reply_data, hashtag_data)
+    stats, results = run_compare_test(reply_data, hashtag_data)
+    all_stats = all_stats.append(pd.DataFrame(stats).transpose())
+    all_results = all_results.append(pd.DataFrame(results).transpose())
+    
+    ## write stats, results to file
+    all_stats.loc[:, 'condition'] = ['hashtags_vs_all', 'hashtags_vs_hashtags', 'replies_vs_all']
+    all_results.loc[:, 'condition'] = ['hashtags_vs_all', 'hashtags_vs_hashtags', 'replies_vs_all']
+    all_stats.transpose().to_csv(stats_out_file, sep='\t', index=True, header=False)
+    all_results.transpose().to_csv(results_out_file, sep='\t', index=True, header=False)
 
 if __name__ == '__main__':
     main()
